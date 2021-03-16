5818e470ab3fad5dfdb500357f5a4eba8ba0fadb
reasat-rafio <rafioar96@gmail.com>
2021-03-16 21:30:42 +0600

    shop page sidebar added

--- src/components/ShopLayout/ShopLayout.tsx
+++ src/components/ShopLayout/ShopLayout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { CategoriesSidebar } from "../CategoriesSidebar/CategoriesSidebar";
-
 import { ProductCategories } from "./ProductCategories";
 
 interface ShopLayoutProps {
